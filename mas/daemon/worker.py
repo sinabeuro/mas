2fd7743077402fb809c9d30f7910fc187b56887b
sinabeuro <ican312@hanmail.net>
2020-08-30 23:08:25 +0900

    daemon: Fix issue of not awaiting coroutine
    Fix a problem where coroutines were not awaited in the Worker class.
    
    Signed-off-by: sinabeuro <ican312@hanmail.net>

--- mas/daemon/worker.py
+++ mas/daemon/worker.py
@@ -8,20 +8,20 @@ class Worker(object):
     def __init__(self, ident, worker, notifier):
         self.ident = ident
         self.worker = worker
-        
+
     async def request(self, theater, day, time, movie, seat, n, silent=True):
-        if self.worker : self.worker.request(theater, day, time, movie, seat, n, silent)
+        if self.worker : await self.worker.request(theater, day, time, movie, seat, n, silent)
         log.info("request is activated")
 
     async def terminate(self, worker_id):
-        if self.worker : self.worker.terminate(worker_id)
+        if self.worker : await self.worker.terminate(worker_id)
         log.info("termination is activated")
 
     async def status(self):
-        if self.worker : self.worker.status()
+        if self.worker : await self.worker.status()
         log.info("status is activated")
         return 0
 
     async def notity(self, msg):
-        if self.worker : self.worker.notify(msg)
+        if self.worker : await self.worker.notify(msg)
         log.info("notity is activated")
