603722fb7c5bde41152173b6cf058b58609265c9
sinabeuro <ican312@hanmail.net>
2020-09-13 19:20:43 +0900

    lib: Add signal handler to Mas
    Signed-off-by: sinabeuro <ican312@hanmail.net>

--- mas/__init__.py
+++ mas/__init__.py
@@ -1,5 +1,7 @@
-from aiozmq import rpc
+import asyncio
+import signal
 
+from aiozmq import rpc
 from structlog import get_logger
 
 from mas.daemon import Service
@@ -13,6 +15,10 @@ class Mas(object):
     def __init__(self, worker=None):
         self.service = None
         self.worker = worker
+        signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+        for s in signals:
+            asyncio.get_event_loop().add_signal_handler(
+                s, lambda s=s: asyncio.create_task(self.shutdown(s)))
 
     async def __aenter__(self):
         return self
@@ -20,11 +26,24 @@ class Mas(object):
     async def __aexit__(self, exc_type, exc_value, traceback):
         if exc_type is not None:
             log.exception(exc_value)
-        self.close()
+        await self.close()
+
+    async def shutdown(self, signal):
+        log.info(f"Received signal {signal.name}...")
+        tasks = [
+            t for t in asyncio.all_tasks()
+            if t is not asyncio.current_task()
+            ]
+        [task.cancel() for task in tasks]
 
     async def start(self):
         self.server = await rpc.serve_rpc(Service(self.worker), bind=ADDRESS)
-        await self.server.wait_closed()
+        try:
+            await self.server.wait_closed()
+        except asyncio.CancelledError:
+            log.info("cancelled service")
+        finally:
+            await self.close()
 
     async def close(self):
         pass
